c9120a5a225672742cb049ee41dbae42230fbfc8
Prasad Nageshkar <prasad.nageshkar@gmail.com>
2022-05-24 14:23:29 +0530

    V1

---
+++ src/components/Charts/index.js
@@ -0,0 +1,7 @@
+import ExampleChart from "./ExampleChart";
+import Column from "./Column";
+import Bar from "./Bar";
+import Pie from "./Pie";
+import Doughnut from "./Doughnut";
+
+export { ExampleChart, Pie, Column, Bar, Doughnut };
